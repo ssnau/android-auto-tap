3429f14cdb2ce36f7f97727d917976eb190fc398
korige <korige@gmail.com>
2016-05-22 20:57:14 +0800

    add random

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var co = require('co');
 var xspawn = require('xspawn');
 function* tap() {
-  return xspawn('adb shell input tap 615 836');
+  var x = 615 + Math.floor(Math.random() * 20)
+  var y = 836 + Math.floor(Math.random() * 20)
+  return xspawn(`adb shell input tap ${x} ${y}`);
 }
 var count = 0;
 function* start() {
